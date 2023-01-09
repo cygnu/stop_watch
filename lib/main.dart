81a1757cbb5fe9fdcbf48586a6bd77291e0ff65f
cygnu <crucia8@icloud.com>
2023-01-09 19:00:50 +0900

    feat: Execute the command 'flutter create stop_watch'

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WatchApp());
+}
+
+class WatchApp extends StatelessWidget {
+  const WatchApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
