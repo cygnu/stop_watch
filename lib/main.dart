9495c59d5c9eb05aab834cea1f95b375f146256a
cygnu <crucia8@icloud.com>
2023-01-10 17:50:10 +0900

    feat: Create function when stopwatch is running

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,9 +9,26 @@ void main() {
 class WatchApp extends StatelessWidget {
   WatchApp({Key? key}) : super(key: key);
   final Stopwatch timer = Stopwatch();
+  final dul = const Duration(milliseconds: 10);
+  String timerDisplay = '00:00:00';
 
   @override
   Widget build(BuildContext context) {
+    void keepRunning() {
+      if (timer.isRunning) {
+        Timer(dul, keepRunning);
+      }
+      int milliSeconds = ((timer.elapsedMilliseconds / 10).floor() % 100);
+      // ignore: prefer_interpolation_to_compose_strings
+      timerDisplay = (timer.elapsed.inHours).toString().padLeft(2, '0') +
+          ':' +
+          (timer.elapsed.inMinutes % 60).toString().padLeft(2, '0') +
+          ':' +
+          (timer.elapsed.inSeconds % 60).toString().padLeft(2, '0') +
+          ':' +
+          (milliSeconds).toString().padLeft(2, '0');
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
